24ada0026a174d480717760823f9dce524663595
rashadsternes <nrsternes@gmail.com>
2025-10-25 00:12:37 -0500

    Setup environment

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl underline">Home</h1>
+      <h1 className="text-white text-3xl underline">Home</h1>
     </div>
   );
 }
