a4ba7b44ccd9e6b680757ab5106c51c930e72e24
rashadsternes <nrsternes@gmail.com>
2025-10-30 23:22:30 -0500

    Add PatientForm component

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,8 @@ import PatientForm from "@/components/forms/PatientForm";
 export default function Home() {
   return (
     <div className="flex h-screen max-h-screen">
+      {/*  TODO: OTP Verification | PasskeyModal */}
+
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
           <Image
