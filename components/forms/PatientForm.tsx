ed0954ea6e39b689977c40e395ed56d4db2919df
rashadsternes <nrsternes@gmail.com>
2025-11-01 22:32:41 -0500

    Add CustomFormField component with form field types

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -4,16 +4,19 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { Button } from "@/components/ui/button";
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
+import { Form } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import CustomFormField from "../CustomFormField";
+
+export enum FormFieldType {
+  INPUT = "imput",
+  TEXTAREA = "textarea",
+  PHONE_INPUT = "phoneInput",
+  CHECKBOX = "checkbox",
+  DATE_PICKER = "datePicker",
+  SELECT = "select",
+  SKELETON = "skeleton",
+}
 
 const formSchema = z.object({
   username: z.string().min(2, {
@@ -39,22 +42,16 @@ const PatientForm = () => {
           <h1 className="header">Hey there 👋🏽</h1>
           <p className="text-dark-700">Schedule your first appointment.</p>
         </section>
-        <FormField
+        <CustomFormField
           control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
+          fieldType={FormFieldType.INPUT}
+          name="name"
+          label="Full"
+          placeholder="John Smith"
+          iconSrc="/assets/icons/user.svg"
+          iconAlt="user"
         />
+
         <Button type="submit">Submit</Button>
       </form>
     </Form>
