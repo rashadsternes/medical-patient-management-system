d066031e67409d60e26fdb5ccbf1427faec0f880
rashadsternes <nrsternes@gmail.com>
2025-11-08 23:20:28 -0600

    Add Register component

---
+++ app/patients/[userId]/register/page.tsx
@@ -0,0 +1,41 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const Register = () => {
+  return (
+    <div className="flex h-screen max-h-screen">
+      {/*  TODO: OTP Verification | PasskeyModal */}
+
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[860px] flex-1 flex-col py-10">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            height={1000}
+            width={1000}
+            alt="patient"
+            className="mb-12 h-10 w-fit"
+          />
+
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+              © 2025 CarePulse
+            </p>
+            <Link href="/?admin=true" className="text-green-500">
+              Admin
+            </Link>
+          </div>
+        </div>
+      </section>
+
+      <Image
+        src="/assets/images/register-img.png"
+        height={1000}
+        width={1000}
+        alt="patient"
+        className="side-img max-w-[390px]"
+      />
+    </div>
+  );
+};
+
+export default Register;
