6e99c008287aa6573fa444efbff0463d1eb2687a
rashadsternes <nrsternes@gmail.com>
2025-11-14 15:56:36 -0600

    Setup Appointment page

--- app/patients/[userId]/new-appointment/page.tsx
+++ app/patients/[userId]/new-appointment/page.tsx
@@ -1,5 +1,35 @@
-const NewAppointment = () => {
-  return <div>NewAppointment</div>;
-};
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import PatientForm from "@/components/forms/PatientForm";
 
-export default NewAppointment;
+export default function NewAppointment() {
+  return (
+    <div className="flex h-screen max-h-screen">
+      {/*  TODO: OTP Verification | PasskeyModal */}
+
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[860px] flex-1 justify-between">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            height={1000}
+            width={1000}
+            alt="patient"
+            className="mb-12 h-10 w-fit"
+          />
+
+          {/* <AppointmentForm /> */}
+          <p className="justify-items-end text-dark-600 xl:text-left">© 2025 CarePulse</p>
+        </div>
+      </section>
+
+      <Image
+        src="/assets/images/appointment-img.png"
+        height={1000}
+        width={1000}
+        alt="appointment"
+        className="side-img max-w-[390px] bg-bottom"
+      />
+    </div>
+  );
+}
