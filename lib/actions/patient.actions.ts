46cb934e899f3990fb7074e72169db56334b6f94
rashadsternes <nrsternes@gmail.com>
2025-11-13 21:08:18 -0600

    Add registerPatient server action with file upload

--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -1,8 +1,18 @@
 "use server";
 
 import { ID, Query } from "node-appwrite";
-import { users } from "../appwrite.config";
+import {
+  PROJECT_ID,
+  DATABASE_ID,
+  PATIENT_TABLE_ID,
+  PUBLIC_BUCKET_ID,
+  PUBLIC_ENDPOINT,
+  databases,
+  storage,
+  users,
+} from "../appwrite.config";
 import { parseStringify } from "../utils";
+import { InputFile } from "node-appwrite";
 
 export const createUser = async (user: CreateUserParams) => {
   try {
@@ -11,15 +21,13 @@ export const createUser = async (user: CreateUserParams) => {
       user.email,
       user.phone,
       undefined,
-      user.username
+      user.name
     );
 
     return parseStringify(newUser);
   } catch (error: any) {
     if (error && error?.code === 409) {
-      const existingUser = await users.list([
-        Query.equal("email", [user.email]),
-      ]);
+      const existingUser = await users.list([Query.equal("email", [user.email])]);
 
       return existingUser.users[0];
     }
@@ -33,5 +41,38 @@ export const getUser = async (userId: string) => {
     return parseStringify(user);
   } catch (error) {
     console.error("An error occurred while fetching the user:", error);
+  }
+};
+
+export const registerPatient = async ({
+  identificationDocument,
+  ...patient
+}: RegisterUserParams) => {
+  try {
+    let file;
+
+    if (identificationDocument) {
+      const inputFile = InputFile.fromBlob(
+        identificationDocument?.get("blobFile") as Blob,
+        identificationDocument?.get("fileName") as string
+      );
+
+      file = await storage.createFile(PUBLIC_BUCKET_ID!, ID.unique(), inputFile);
+    }
+
+    const newPatient = await databases.createDocument(
+      DATABASE_ID!,
+      PATIENT_TABLE_ID!,
+      ID.unique(),
+      {
+        identificationDocumentId: file?.$id || null,
+        identificationDocumentUrl: `${PUBLIC_ENDPOINT}/storage/buckets/${PUBLIC_BUCKET_ID}/files/${file?.$id}/view?project=${PROJECT_ID}`,
+        ...patient,
+      }
+    );
+
+    return parseStringify(newPatient);
+  } catch (error) {
+    console.log(error);
   }
 };
