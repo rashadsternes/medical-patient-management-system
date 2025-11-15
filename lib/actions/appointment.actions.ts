e7f91629382dd26f7bcd9a6b4c698bb928369941
rashadsternes <nrsternes@gmail.com>
2025-11-15 16:43:36 -0600

    Add createAppointment server action

---
+++ lib/actions/appointment.actions.ts
@@ -0,0 +1,18 @@
+import { databases, DATABASE_ID, APPOINTMENT_TABLE_ID } from "@/lib/appwrite.config";
+import { ID } from "node-appwrite";
+import { parseStringify } from "@/lib/utils";
+
+export const createAppointment = async (appointment: CreateAppointmentParams) => {
+  try {
+    const newAppointment = await databases.createDocument(
+      DATABASE_ID!,
+      APPOINTMENT_TABLE_ID!,
+      ID.unique(),
+      appointment
+    );
+
+    return parseStringify(newAppointment);
+  } catch (error) {
+    console.error("An error occurred while creating the appointment:", error);
+  }
+};
