44fcbebf4839f59e095cbaaf3665d8fef5c255d3
rashadsternes <nrsternes@gmail.com>
2025-11-18 15:28:32 -0600

    Add admin modal to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,13 @@ import Image from "next/image";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import PatientForm from "@/components/forms/PatientForm";
+import PassKeyModal from "@/components/PasskeyModal";
 
-export default function Home() {
+export default function Home({ searchParams }: SearchParamProps) {
+  const isAdmin = searchParams.admin === "true";
   return (
     <div className="flex h-screen max-h-screen">
-      {/*  TODO: OTP Verification | PasskeyModal */}
+      {isAdmin && <PassKeyModal />}
 
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
@@ -20,9 +22,7 @@ export default function Home() {
 
           <PatientForm />
           <div className="text-14-regular mt-20 flex justify-between">
-            <p className="justify-items-end text-dark-600 xl:text-left">
-              © 2025 CarePulse
-            </p>
+            <p className="justify-items-end text-dark-600 xl:text-left">© 2025 CarePulse</p>
             <Link href="/?admin=true" className="text-green-500">
               Admin
             </Link>
---
+++ components/PasskeyModal.tsx
@@ -0,0 +1,53 @@
+"use client";
+import { useState } from "react";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+const PasskeyModal = () => {
+  const router = useRouter();
+  const [open, setOpen] = useState(true);
+  const closeModal = () => {
+    setOpen(false);
+    router.push("/");
+  };
+
+  return (
+    <AlertDialog open={open} onOpenChange={setOpen}>
+      <AlertDialogContent className="shad-alert-dialog">
+        <AlertDialogHeader>
+          <AlertDialogTitle className="flex items-start justify-between">
+            Admin Access Verification
+            <Image
+              src="/assets/icons/close.svg"
+              height={20}
+              width={20}
+              alt="close"
+              className="cursor-pointer"
+              onClick={() => closeModal()}
+            />
+          </AlertDialogTitle>
+          <AlertDialogDescription>
+            To access the admin page, please enter the passkey.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction>Continue</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
+
+export default PasskeyModal;
