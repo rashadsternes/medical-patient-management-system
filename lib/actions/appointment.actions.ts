e17898bef8d32ae9d3d29b5b6695578ec0edf78e
rashadsternes <nrsternes@gmail.com>
2025-11-19 07:44:31 -0600

    Add appointment action to fetch and count appointments by status

--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -1,8 +1,9 @@
 "use server";
 
 import { databases, DATABASE_ID, APPOINTMENT_TABLE_ID } from "@/lib/appwrite.config";
-import { ID } from "node-appwrite";
+import { ID, Query } from "node-appwrite";
 import { parseStringify } from "@/lib/utils";
+import { Appointment } from "@/types/appwrite.types";
 
 export const createAppointment = async (appointment: CreateAppointmentParams) => {
   try {
@@ -28,6 +29,41 @@ export const getAppointment = async (appointmentId: string) => {
     );
 
     return parseStringify(appointment);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getRecentAppointment = async () => {
+  try {
+    const appointments = await databases.listDocuments(DATABASE_ID!, APPOINTMENT_TABLE_ID!, [
+      Query.orderDesc("$createdAt"),
+    ]);
+
+    const initialCounts = {
+      scheduledCount: 0,
+      pendingCount: 0,
+      cancelledCount: 0,
+    };
+
+    const counts = (appointments.documents as Appointment[]).reduce((acc, appointment) => {
+      if (appointment.status === "scheduled") {
+        acc.scheduledCount += 1;
+      } else if (appointment.status === "pending") {
+        acc.pendingCount += 1;
+      } else if (appointment.status === "cancelled") {
+        acc.cancelledCount += 1;
+      }
+      return acc;
+    }, initialCounts);
+
+    const data = {
+      totalCount: appointments.total,
+      ...counts,
+      documents: appointments.documents,
+    };
+
+    return parseStringify(data);
   } catch (error) {
     console.log(error);
   }
