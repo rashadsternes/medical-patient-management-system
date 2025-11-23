508bb44fb93406f9c5bdfde49df2c55ffb772161
rashadsternes <nrsternes@gmail.com>
2025-11-22 23:46:27 -0600

    Added minor fix to smsMessage

--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -103,7 +103,9 @@ export const updateAppointment = async ({
       Hi, it's CarePulse.
       ${
         type === "schedule"
-          ? `Your appointment has been scheduled for ${formatDateTime(appointment.schedule!)}`
+          ? `Your appointment has been scheduled for 
+          ${formatDateTime(appointment.schedule!).dateTime}
+          with Dr. ${appointment.primaryPhysician}.`
           : `We regret to inform that your appointment for has been cancelled for the
       following reason: ${appointment.cancellationReason}`
       }`;
